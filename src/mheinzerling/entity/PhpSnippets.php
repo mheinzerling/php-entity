e3c4559e8dbfae21b64830afea3929c927304ab1
Martin Heinzerling <webmaster@mheinzerling.de>
2014-08-16 14:25:52 +0200

    fix "no primary key"

--- src/mheinzerling/entity/PhpSnippets.php
+++ src/mheinzerling/entity/PhpSnippets.php
@@ -91,7 +91,8 @@ class PhpSnippets
         $result .= "            'table' => '" . strtolower($name) . "',\n";
         $result .= "            'fields' => array(\n";
         $result .= "" . $fields . "),\n";
-        $result .= "            'pk' => array('" . implode("', '", $pk) . "'),\n";
+		$pks="'" . implode("', '", $pk) . "'";
+        $result .= "            'pk' => array(".(count($pk)>0?$pks:'')."),\n";
         $result .= "            'unique' => array(" . $unique . "),\n";
         $result .= "            'autoincrement' => " . ($auto == null ? "null" : "'$auto'") . "\n";
         $result .= "        );\n";
